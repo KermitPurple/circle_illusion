13ee24d6b935065e6e344cd8ef564c2e9ffa9aba
Shane McDonough <shanemcdman@gmail.com>
2022-01-11 17:11:53 -0800

    Show circles

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 let els = {
     'circle_count': document.querySelector('#circle-count'),
 };
+let tracks = [];
+let radius;
 
 function setup(){
     createCanvas(windowWidth, windowHeight);
+    angleMode(DEGREES);
+    radius = min(windowWidth, windowHeight) * 0.4;
+    generate_tracks();
+    els.circle_count.addEventListener('input', generate_tracks);
 }
 
 function draw(){
     translate(windowWidth / 2, windowHeight / 2);
     background(0);
+    for(let track of tracks){
+        track.update();
+        track.draw_line();
+    }
+    for(let track of tracks)
+        track.draw_circle();
 }
 
 function windowResized(){
     resizeCanvas(windowWidth, windowHeight);
+    radius = min(windowWidth, windowHeight) * 0.4;
+    for(let track of tracks)
+        track.radius = radius;
+}
+
+function generate_tracks(){
+    tracks = Track.generate(
+        parseInt(els.circle_count.value),
+        radius
+    );
 }
--- track.js
+++ track.js
@@ -1,54 +1,96 @@
 class Track{
     constructor(
-        theta,
         offset,
         radius,
         color_fn = null,
+        line_color_fn = null,
         center = null
     ){
-        this.theta = theta;
+        this.theta = offset;
         this.offset = offset;
         this.radius = radius;
-        this.color_fn = color_fn ?? degrees => {
+        this.color_fn = color_fn ?? function(degrees) {
             push();
             colorMode(HSL)
-            let c = color(degrees, 100 50);
+            let c = color(degrees, 100, 50);
             pop();
-            return c;
+            return [color('white'), c];
+        }
+        this.line_color_fn = line_color_fn ?? function(_degrees){
+            let c = color('grey');
+            return [c, c];
         }
         this.center = center ?? createVector(0, 0);
     }
 
     draw_line(){
-        let start = rotate(
+        let start = rotate_around(
             this.offset,
             createVector(
-                Math.floor(this.center.x + this.radius),
+                this.center.x + this.radius,
                 this.center.y
             ),
             this.center
         );
-        let end = rotate(
+        let end = rotate_around(
             this.offset,
             createVector(
-                Math.floor(this.center.x - this.radius),
+                this.center.x - this.radius,
                 this.center.y
             ),
             this.center
         );
-        line(start, end);
+        let [s, f] = this.line_color_fn(this.tehta);
+        stroke(s);
+        fill(f);
+        line(
+            start.x,
+            start.y,
+            end.x,
+            end.y,
+        );
+        let d = 3;
+        circle(start.x, start.y, d);
+        circle(end.x, end.y, d);
     }
 
     draw_circle(){
+        let pos = rotate_around(
+            this.offset,
+            createVector(
+                this.radius * cos(this.theta) + this.center.x,
+                this.center.y
+            ),
+            this.center
+        );
+        let [s, f] = this.color_fn(this.theta);
+        stroke(s);
+        fill(f);
+        circle(pos.x, pos.y, 20);
     }
 
-    draw(){
+    update(){
+        this.theta += 1;
+        this.theta %= 360;
+    }
+
+    static generate(count, radius){
+        console.log(count);
+        let tracks = [];
+        for(let i = 0; i < count; i++){
+            let angle = i / count * 180
+            tracks.push(new Track(
+                angle,
+                radius
+            ));
+        }
+        return tracks;
     }
 }
 
-function rotate(angle, point, center = null){
+function rotate_around(angle, point, center = null){
     return createVector(
-        Math.floor(center.x + Math.cos(angle) * (point.x - center.x) - Math.sin(angle) * (point.y - center.y)),
-        Math.floor(center.y + Math.sin(angle) * (point.x - center.x) + Math.cos(angle) * (point.y - center.y)),
+        center.x + cos(angle) * (point.x - center.x) - sin(angle) * (point.y - center.y),
+        center.y + sin(angle) * (point.x - center.x) + cos(angle) * (point.y - center.y),
     );
 }
